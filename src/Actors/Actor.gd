f14618c2af4abf3d8b52a5009626d66282033099
Abdullah Nawaz Khan <36707277+abdullahnawazkhan@users.noreply.github.com>
2021-01-26 20:57:53 +0500

    Delete src directory

--- src/Actors/Actor.gd
+++
@@ -1,9 +0,0 @@
-extends KinematicBody2D
-class_name actor
-
-const FLOOR_NORMAL := Vector2.UP
-
-export var gravity := 4000.0
-export var speed := Vector2(300.0, 1400.0) 
-
-var _velocity := Vector2.ZERO
--- src/ui/resume_button.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-onready var ui := get_tree().get_current_scene().get_node("user_interface/user_interface")
-
-
-func _on_button_up() -> void:
-	get_tree().paused = false
-	ui.is_paused = false
