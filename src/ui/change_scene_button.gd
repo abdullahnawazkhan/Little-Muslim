cf7fbcdf97d1e8e4691035dc40ffedcdaee8dcd2
abdullahnawazkhan <abdullah.nawazkha@gmail.com>
2021-01-26 21:18:22 +0500

    Fixed issue with project files being case sensitive     - previous entire project was deleted     - new project was created with fix and being uploaded

---
+++ src/ui/change_scene_button.gd
@@ -0,0 +1,17 @@
+tool
+extends Button
+
+export(String, FILE) var scene_path := ""
+
+func _get_configuration_warning() -> String:
+	if not scene_path:
+		return "The next scene property can't be empty"
+	else:
+		return ""
+
+
+func _on_change_scene_button_button_up() -> void:
+	PlayerData.score = 0
+	PlayerData.deaths = 0
+	get_tree().change_scene(scene_path)
+	get_tree().paused = false
