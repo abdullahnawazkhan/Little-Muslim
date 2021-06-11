491c20ffc1af8afb8a3b2906829f837b97b92f67
abdullahnawazkhan <abdullah.nawazkha@gmail.com>
2021-06-11 23:41:52 +0500

    Things implemented:     - Added proper permission checker 	- No longer getting issue, "need to restart app to see affect of 	permission acceptance"

--- src/qibla_finder/qibla_finder.gd
+++ src/qibla_finder/qibla_finder.gd
@@ -4,23 +4,47 @@ var qibla_location_finder
 
 var direction
 
+var is_location_on
+
 
 func _ready() -> void:
 	if Engine.has_singleton("QiblaLocationFinder"):
 		qibla_location_finder = Engine.get_singleton("QiblaLocationFinder")
-		print("Qibla Connection DONE")
+		
+		if qibla_location_finder.get_location_on() == true:
+			is_location_on = true
+		else:
+			$error_msg.visible = true
+			is_location_on = false
+		
+		
 
 
 func _process(delta: float) -> void:
-	var new_direction = qibla_location_finder.getDirection()
-	if (new_direction != null):
-		if ($ColorRect.visible == true && new_direction != 0.0):
-			$ColorRect.visible = false
+	if qibla_location_finder.get_location_on() == true:
+		is_location_on = true
+	else:
+		$error_msg.visible = true
+		is_location_on = false
 
-		if (new_direction != direction):
-			direction = new_direction
-			$qibla/Sprite.rotation_degrees = direction - 90
+	if is_location_on == true:
+		var new_direction = qibla_location_finder.getDirection()
+		if (new_direction != null):
+			if ($ColorRect.visible == true && new_direction != 0.0):
+				$ColorRect.visible = false
+
+			if (new_direction != direction):
+				direction = new_direction
+				$qibla/Sprite.rotation_degrees = direction - 90
 
 
 func _on_TouchScreenButton_pressed() -> void:
 	queue_free()
+
+
+func _on_Button_button_up() -> void:
+	if qibla_location_finder.get_location_on() == true:
+		is_location_on = true
+		$error_msg.visible = false
+	else:
+		is_location_on = false
--- src/screens/start_up_internet_checker/internet_checker.gd
+++ src/screens/start_up_internet_checker/internet_checker.gd
@@ -1,17 +1,35 @@
 extends Control
 
+var permission_checker
+
+var asked_user_for_permissions = false
 
 func _ready() -> void:
 	$HTTPRequest.request("https://example.com/")
+	
+	if Engine.has_singleton("PermissionChecker"):
+		permission_checker = Engine.get_singleton("PermissionChecker")
 
 
 func _on_HTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
 	if (response_code == 200):
 		InternetConnection.started = true
-		get_tree().change_scene("res://src/screens/log_checker/login_checker.tscn")
+#		get_tree().change_scene("res://src/screens/log_checker/login_checker.tscn")
+		check_for_permissions()
 	else:
 		$ColorRect/error_msg.visible = true
 
 
 func _on_Button_button_up() -> void:
 	$HTTPRequest.request("https://example.com/")
+
+
+func check_for_permissions():
+	permission_checker.ask_for_permissions()
+	asked_user_for_permissions = true
+
+
+func _process(delta: float) -> void:
+	if (asked_user_for_permissions == true):
+		if permission_checker.permission_accepted_status() == true:
+			get_tree().change_scene("res://src/screens/log_checker/login_checker.tscn")
